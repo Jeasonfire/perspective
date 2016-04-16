92e814b9aca510f3ebe71709bdba995a158828ef
Jeasonfire <jeasonfire@gmail.com>
2016-04-16 19:07:19 +0300

    2D perspective implemented.

---
+++ Assets/Scripts/PerspectiveController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PerspectiveController : MonoBehaviour {
+    public const int PERSPECTIVE_2D = 0;
+    public const int PERSPECTIVE_3D = 1;
+    public const int INITIAL_PERSPECTIVE = PERSPECTIVE_2D;
+    public static int CURRENT_PERSPECTIVE = INITIAL_PERSPECTIVE;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public CharacterController character;
+    public Transform eye;
+    public float movementSpeed;
+
+    private float currentFallingSpeed = 0;
+
+    void Start () {
+
+	}
+
+	void Update () {
+        Vector3 movement = new Vector3();
+        Vector3 rotation = new Vector3();
+        switch (PerspectiveController.CURRENT_PERSPECTIVE) {
+            case PerspectiveController.PERSPECTIVE_2D:
+                movement.x = Input.GetAxis("Horizontal");
+                break;
+            case PerspectiveController.PERSPECTIVE_3D:
+                movement.x = Input.GetAxis("Horizontal");
+                movement.z = Input.GetAxis("Vertical");
+                movement = eye.TransformDirection(movement).normalized;
+                break;
+        }
+        if (!character.isGrounded) {
+            this.currentFallingSpeed += Physics.gravity.y * Time.deltaTime;
+        } else {
+            this.currentFallingSpeed = 0;
+        }
+        character.Move((movement * this.movementSpeed + new Vector3(0, this.currentFallingSpeed, 0)) * Time.deltaTime);
+        this.eye.localEulerAngles = this.eye.localEulerAngles + rotation;
+    }
+}
